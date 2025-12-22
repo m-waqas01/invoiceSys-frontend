7c321e750abc5be681fc3e86deeba890873888c7
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-12-22 14:41:34 +0000

    first commit

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.API_BASE_URL || "http://localhost:5000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
